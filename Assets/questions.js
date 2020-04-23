fdac17a370d7a81701e733db66b9c05029c661fb
Blake <Blakeathompson94@gmail.com>
2020-04-23 17:58:39 -0400

    added first function and event listener

---
+++ Assets/questions.js
@@ -0,0 +1,14 @@
+var quizQuestions = [
+    {
+        question: "What is the purpose Bootstrap",
+        choices: ["Space ship", "9", "pizza", "110"],
+        answer: "Space ship"
+    },
+    {
+        question: "Where did the sun come from",
+        choices: ["mars", "dogs", "pizza", "110"],
+        answer: "mars"
+    },
+]
+
+// for each loop to display all questions 
---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+var startBtnElement = document.querySelector(".start-button");
+
+function startQuiz() {
+    // console.log(quizQuestions[0].question);
+    // console.log(quizQuestions[0].answer);
+    // hide initial screen
+
+    //show the first question => we need a function that's going to getQuestions
+
+    //start the timer - we need a timer function track time
+
+}
+
+
+startBtnElement.addEventListener("click", startQuiz);
