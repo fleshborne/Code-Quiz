074863229c62dedca45a3602977783f644340bfc
Blake <Blakeathompson94@gmail.com>
2020-04-26 23:45:56 -0400

    still working through it, added visual for readme, will need more time to finish

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,7 @@ var timeSetup = document.querySelector(".nav-text");
 var questionEl = document.querySelector("#question");
 var questionElContainer = document.querySelector("#question-container")
 var answerBtnElement = document.querySelector("#answer-buttons");
+var count = 75;
 
 
 let shuffledQuestions, currentQuestionIndex
@@ -14,17 +15,18 @@ nextBtn.addEventListener("click", () => {
     currentQuestionIndex++
     setNextQuestion()
 })
-
+// get the next question
 function setNextQuestion() {
     resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+// go back to default after each question
 function resetState() {
     clearStatusClass(document.body)
     nextBtn.classList.add('hide')
-    while (questionEl.firstChild) {
-        answerBtnElement.removeChild(questionEl.firstChild)
+    while (answerBtnElement.firstChild) {
+        answerBtnElement.removeChild(answerBtnElement.firstChild)
     }
 }
 function showQuestion(question) {
@@ -52,8 +54,10 @@ function selectAnswer(e) {
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextBtn.classList.remove('hide')
     } else {
-        startBtn.innerText = 'Restart'
+        startBtn.innerText = 'Save your Score!'
+        nextBtn.innerText = 'Restart!'
         startBtn.classList.remove('hide')
+        nextBtn.classList.remove('hide')
     }
 }
 
@@ -70,7 +74,7 @@ function startQuiz() {
     currentQuestionIndex = 0;
 
     //    variable to store the current count, set to 0. Decrement the current count
-    var count = 75;
+
     //    start the timer - we need a timer function track time setInterval of 75000 ms
 
 
@@ -102,17 +106,31 @@ function startQuiz() {
     setNextQuestion();
 
 };
-
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
 
+function calculateScore() {
+    return Math.round(correct * 2);
+}
 
 function endQuiz() {
     // call this inside interval
     // call this when on last question
     // alert("The Quiz has Ended!")
+    clearInterval(timeSetup);
+    var score = calculateScore();
+    var scoreElement = document.querySelector("#score");
+    scoreElement.classList.remove('hide');
     return;
 
 
@@ -125,7 +143,9 @@ var questions = [
             { text: "9", correct: false },
             { text: "pizza", correct: true },
             { text: "110", correct: false },
-        ]
+        ],
+        score: 1
+
 
     },
     {
